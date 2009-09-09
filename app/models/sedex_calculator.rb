6a59ef5933cc21829f9a4433627bb3a584ea8ac6
Rodrigo <rodrigo.ortiz@locaweb.com.br>
2009-09-09 12:52:16 -0300

    Teste para verificar a soma do peso com mais de uma quantidade

--- app/models/sedex_calculator.rb
+++ app/models/sedex_calculator.rb
@@ -1,16 +1,27 @@
 class SedexCalculator < Calculator
+preference :cep_origem, :string
 
   def compute(order=nil)
     return 0 if order.nil? || order.empty?
 
     peso_total = order.sum do |item|
-      item.variant.weight ? item.variant.weight : 0
+      item.variant.weight ? item.variant.weight * item.quantity : 0
     end
 
     return 0 if peso_total == 0
 
+    parameters = {
+      :nCdEmpresa => '',
+      :sDsSenha => '',
+      :nCdServico => '40010', # Código do Sedex
+      :sCdMaoPropria => 'N',
+      :sCdAvisoRecebimento => 'N',
+      :nVlPeso => peso_total.to_s,
+      :sCepOrigem => preferred_cep_origem
+    }
+
     ws = CalcPrecoPrazoWSSoap.new
-    result = ws.calcPrecoPrazo :nVlPeso => '1'
+    result = ws.calcPrecoPrazo parameters
     result.calcPrecoPrazoResult.servicos.first.valor
 
   end
--- spec/models/sedex_calculator_spec.rb
+++ spec/models/sedex_calculator_spec.rb
@@ -1,11 +1,10 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 
-describe "SedexCalculator" do
+describe "SedexCalculator", "When there is no weight" do
 
   before(:each) do
     @calculator = SedexCalculator.new
-    @ws = mock_model CalcPrecoPrazoWSSoap
-    CalcPrecoPrazoWSSoap.stub(:new).with(no_args()).and_return(@ws)
+    CalcPrecoPrazoWSSoap.should_not_receive(:new)
   end
 
   it "should return zero when compute receive nil" do
@@ -19,26 +18,83 @@ describe "SedexCalculator" do
   end
 
   it "should return zero when compute receive products whithout weight" do
-    variant = mock_model Variant, :weight => nil
-    line_item = mock_model LineItem, :variant => variant
-
-    result = @calculator.compute [line_item]
+    result = @calculator.compute [mock_line_item :weight => nil]
     assert_equal 0, result
   end
 
-  it "should return the ws result based on the weight sum" do
-    variant = mock_model Variant, :weight => 1
-    line_item = mock_model LineItem, :variant => variant
+end
 
-    valor = mock 'valor', :valor => 6
+describe "SedexCalculator", "When there is weight" do
+
+  before(:each) do
+    @calculator = SedexCalculator.new
+    @ws = mock_model CalcPrecoPrazoWSSoap
+    CalcPrecoPrazoWSSoap.stub(:new).with(no_args()).and_return(@ws)
+
+    valor = mock 'valor', :valor => @valor_ws_esperado
     calcPrecoPrazoResult = mock 'calcPrecoPrazoResult', :servicos => [valor]
-    result = mock 'result', :calcPrecoPrazoResult => calcPrecoPrazoResult
+    @result = mock 'result', :calcPrecoPrazoResult => calcPrecoPrazoResult
+  end
 
-    @ws.stub!(:calcPrecoPrazo).with(hash_including(:nVlPeso => '1')).and_return(result)
+  it "should return the ws result based on the weight sum of one variant" do
+    @ws.should_receive(:calcPrecoPrazo).with(hash_including(:nVlPeso => '1')).and_return(@result)
 
-    assert_equal 6, @calculator.compute([line_item])
+    line_item = mock_line_item :weight => 1
 
+    assert_equal @valor_ws_esperado, @calculator.compute([line_item])
+  end
+
+  it "should return the ws result based on the weight sum of two different variants" do
+    @ws.should_receive(:calcPrecoPrazo).with(hash_including(:nVlPeso => '10')).and_return(@result)
+
+    ipod = mock_line_item :weight => 6
+    wii = mock_line_item :weight => 4
+
+    assert_equal @valor_ws_esperado, @calculator.compute([ipod, wii])
+  end
+
+  it "should return the ws result based on the weight sum of one variant where the quantity is two" do
+    @ws.should_receive(:calcPrecoPrazo).with(hash_including(:nVlPeso => '12')).and_return(@result)
+
+    ipod = mock_line_item :weight => 6, :quantity => 2
+
+    assert_equal @valor_ws_esperado, @calculator.compute([ipod])
+  end
+
+  it "should send default parameters" do
+    parameters = {
+      :nCdEmpresa => '',
+      :sDsSenha => '',
+      :nCdServico => '40010', # Código do Sedex
+      :sCdMaoPropria => 'N',
+      :sCdAvisoRecebimento => 'N'
+    }
+
+    @ws.should_receive(:calcPrecoPrazo).with(hash_including(parameters)).and_return(@result)
+
+    @calculator.compute([mock_line_item(:weight => 5)])
+  end
+
+  it "should use the preferred_cep_origem" do
+    @calculator.should_receive(:preferred_cep_origem).and_return('04543900')
+
+    @ws.should_receive(:calcPrecoPrazo).with(hash_including(:sCepOrigem => '04543900')).and_return(@result)
+
+    @calculator.compute([mock_line_item(:weight => 4)])
   end
 
 end
+
+private
+
+  def mock_line_item(options)
+
+    defaults = {:weight => 0, :quantity => 1}
+
+    options = defaults.merge options
+
+    variant = mock_model Variant, :weight => options[:weight]
+    line_item = mock_model LineItem, :variant => variant, :quantity => options[:quantity]
+
+  end
 
